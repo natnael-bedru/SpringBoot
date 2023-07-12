f3b4e4b782775967c1ae679f4e6c4d7cfb50d6e0
Nati <natnaelbedru@gmail.com>
2023-07-12 14:58:38 +0300

    Swagger 3.0.0 integration
    Added Dependencies
          [+] io.springfox springfox-boot-starter V3.0.0
          [+] io.swagger.core.v3 swagger-annotations V2.1.9

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,15 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.persistence.*;
-import java.time.LocalDate;
-import java.util.List;
 
-@EnableJpaRepositories
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableSwagger2    // swager nessesary annotations
+@EnableWebMvc      // swager nessesary annotations
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/example/demo/OrderController.java
+++ src/main/java/com/example/demo/OrderController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
